e22e744313ba70a6ad7c60feb777bb4d7c783029
Staszek <ztach@poczta.onet.pl>
2019-06-15 01:30:39 +0200

    Updates

---
+++ precache-manifest.455f7ef8661a5622a4ed0824232e7ef6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d0e931a053a06bacaeefa4e8a04e7d9",
+    "url": "/React_app/static/media/header5.9d0e931a.jpg"
+  },
+  {
+    "revision": "37914402f1a4fb66d3c88d07237c6e58",
+    "url": "/React_app/static/media/header4.37914402.jpg"
+  },
+  {
+    "revision": "2680c10a99f728e49cbf986dee83942c",
+    "url": "/React_app/static/media/header3.2680c10a.jpg"
+  },
+  {
+    "revision": "b2addb52f905e7ca61464642abea8756",
+    "url": "/React_app/static/media/header2.b2addb52.jpg"
+  },
+  {
+    "revision": "e6a1e0c659a6ba3fc68a09e606701124",
+    "url": "/React_app/static/media/header1.e6a1e0c6.jpg"
+  },
+  {
+    "revision": "707a289c9208e9fb0b3f",
+    "url": "/React_app/static/js/runtime~main.7eb74666.js"
+  },
+  {
+    "revision": "c02d2c605d876fecae0f",
+    "url": "/React_app/static/js/main.8230bf6c.chunk.js"
+  },
+  {
+    "revision": "d1789a4294350fcc14b0",
+    "url": "/React_app/static/js/2.0d6823db.chunk.js"
+  },
+  {
+    "revision": "c02d2c605d876fecae0f",
+    "url": "/React_app/static/css/main.c31ea1fc.chunk.css"
+  },
+  {
+    "revision": "d49292dbf332bcaeb942d80d0310f956",
+    "url": "/React_app/index.html"
+  }
+];
